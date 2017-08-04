a3eb6e3d525349a34887c7918ab8c2d960224ae4
nachumFreedman <nochum09@gmail.com>
2017-08-04 12:29:41 +0300

    calc update

--- src/CalcInput.js
+++ src/CalcInput.js
@@ -2,30 +2,48 @@ import React, {Component} from 'react';
 class CalcInput extends Component{
   render(){
     return (
-      <div className="calc-input">
-      <button onClick={() => (this.props.onNumber(1))}> 1 </button>
-      <button onClick={() => (this.props.onNumber(2))}> 2 </button>
-      <button onClick={() => (this.props.onNumber(3))}> 3 </button>
-      <button onClick={() => (this.props.onFunc('*'))}> X </button>
       <div>
-      <button onClick={() => (this.props.onNumber(4))}> 4 </button>
-      <button onClick={() => (this.props.onNumber(5))}> 5 </button>
-      <button onClick={() => (this.props.onNumber(6))}> 6 </button>
-      <button onClick={() => (this.props.onFunc('+'))}> + </button>
+      <div>
+      <button onClick={() => (this.props.onNumber(1))}
+      className='button1'> 1 </button>
+      <button onClick={() => (this.props.onNumber(2))}
+      className='button2'> 2 </button>
+      <button onClick={() => (this.props.onNumber(3))}
+      className='button3'> 3 </button>
+      <button onClick={() => (this.props.onFunc('*'))}
+      className='buttonTimes'> X </button>
       </div>
       <div>
-      <button onClick={() => (this.props.onNumber(7))}> 7 </button>
-      <button onClick={() => (this.props.onNumber(8))}> 8 </button>
-      <button onClick={() => (this.props.onNumber(9))}> 9 </button>
-      <button onClick={() => (this.props.onFunc('-'))}> - </button>
+      <button onClick={() => (this.props.onNumber(4))}
+      className='button4'> 4 </button>
+      <button onClick={() => (this.props.onNumber(5))}
+      className='button5'> 5 </button>
+      <button onClick={() => (this.props.onNumber(6))}
+      className='button6'> 6 </button>
+      <button onClick={() => (this.props.onFunc('+'))}
+      className='buttonPlus'> + </button>
       </div>
-      <button onClick={this.props.onClear}> C </button>
-      <button onClick={() => (this.props.onNumber(0))}> 0 </button>
-      <button onClick={() => (this.props.onFunc('/'))}> / </button>
-      <button onClick={() => (this.props.onFunc('%'))}> % </button>
-      <button onClick={this.props.onEq}> Is </button>
+      <div>
+      <button onClick={() => (this.props.onNumber(7))}
+      className='button7'> 7 </button>
+      <button onClick={() => (this.props.onNumber(8))}
+      className='button8'> 8 </button>
+      <button onClick={() => (this.props.onNumber(9))}
+      className='button9'> 9 </button>
+      <button onClick={() => (this.props.onFunc('-'))}
+      className='buttonMinus'> - </button>
       </div>
-
+      <button onClick={this.props.onClear}
+      className='buttonClear'> C </button>
+      <button onClick={() => (this.props.onNumber(0))}
+      className='button0'> 0 </button>
+      <button onClick={() => (this.props.onFunc('/'))}
+      className='buttonDevide'> / </button>
+      <button onClick={() => (this.props.onFunc('%'))}
+      className='buttonMod'> % </button>
+      <button onClick={this.props.onEq}
+      className='buttonEq'> Is </button>
+      </div>
     )
   }
 }
